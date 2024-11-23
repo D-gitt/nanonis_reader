4c14b6b8d15a3ff72a58615637abe62f03bdd775
Dowook Kim <dw.kim@postech.ac.kr>
2024-11-23 13:20:51 +0900

    [UPDATE] Delete 'warnings' and 'math' in install_requires. Actually was 'warning' (typo).

--- setup.py
+++ setup.py
@@ -2,18 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='nanonis_reader',
-    version='0.0.1',
+    version='0.0.2',
     description='A Python package for reading STM experimental data files obtained from Nanonis, based on nanonispy',
     author='Dowook Kim',
     author_email='dw.kim@postech.ac.kr',
     url='https://github.com/D-gitt/nanonis_reader',
     install_requires=[
         'numpy',
-        'matplotlib',  # 시각화를 위한 라이브러리
-        'nanonispy',    # 필수 의존성 패키지
-        'scipy',
-        'warning',
-        'math'
+        'matplotlib',
+        'nanonispy',
+        'scipy'
     ],
     packages=find_packages(exclude=[]),
     keywords=['nanonis', 'reader', 'nanonispy', 'STM data', 'scientific data analysis'],
