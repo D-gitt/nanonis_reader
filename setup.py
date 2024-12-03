69d3cebc117def7c36c9e314dfd68ca56b77f7b0
Dowook Kim <dw.kim@postech.ac.kr>
2024-12-03 12:30:54 +0900

    [UPDATE] Ver. 0.0.8: Add specific code to setup.py for writing PyPI project description.

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
 from setuptools import setup, find_packages
 
+# Load the contents of the README.md file.
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='nanonis_reader',
-    version='0.0.7',
+    version='0.0.8',
     description='A Python package for reading STM experimental data files obtained from Nanonis, based on nanonispy',
+    long_description=long_description,  # Set long_description
+    long_description_content_type='text/markdown',  # Specify Markdown format
     author='Dowook Kim',
     author_email='dw.kim@postech.ac.kr',
     url='https://github.com/D-gitt/nanonis_reader',
     install_requires=[
-        'numpy',
         'matplotlib',
         'nanonispy',
-        'scipy',
-        'python-pptx'
+        'numpy',
+        'python-pptx',
+        'scipy'
     ],
     packages=find_packages(exclude=[]),
     keywords=['nanonis', 'reader', 'nanonispy', 'STM data', 'scientific data analysis'],
     python_requires='>=3.6',
-    package_data={},
-    zip_safe=False,
     classifiers=[
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
