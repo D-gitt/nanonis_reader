a8a5e4dfdced5ec71b78df43d855f7c08aada225
Dowook Kim <dw.kim@postech.ac.kr>
2025-04-10 02:32:43 +0900

    [UPDATE] Rename load.py as base.py for clarity.

--- nanonis_reader/__init__.py
+++ nanonis_reader/__init__.py
@@ -1,8 +1,8 @@
 # import nanonis_reader 실행 시 자동으로 하위 module 들을 사용할수 있도록 미리 import.
-from nanonis_reader import sxm, dat, grid, load, nanonis_sxm, nanonis_dat, nanonis_3ds, cmap_custom, find_value, schematic, spectral_analysis, atom_analysis, util
+from nanonis_reader import base, sxm, dat, grid, nanonis_sxm, nanonis_dat, nanonis_3ds, cmap_custom, find_value, schematic, spectral_analysis, atom_analysis, util
 
 # 중복 타이핑 방지용 import (nr.load instead of nr.load.load).
-from .load import load
+from .base import load
 
 # from nanonis_reader import *에서 *에 포함되는 것들. (* : "__all__에 포함된 것을 전부" import)
-__all__ = ['sxm', 'dat', 'grid', 'load', 'nanonis_sxm', 'nanonis_dat', 'nanonis_3ds', 'cmap_custom', 'find_value', 'schematic', 'spectral_analysis', 'atom_analysis', 'util']
+__all__ = ['sxm', 'dat', 'grid', 'base', 'nanonis_sxm', 'nanonis_dat', 'nanonis_3ds', 'cmap_custom', 'find_value', 'schematic', 'spectral_analysis', 'atom_analysis', 'util']
